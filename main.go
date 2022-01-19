92e7cb9f14cc2329183bf6f8e8169edb5ed4ec09
Md Ferdous Alam <56306765+mdferdousalam@users.noreply.github.com>
2022-01-19 11:57:30 +0600

    version 6

--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import "fmt"
 func main() {
 	var name string = "ferdous"
 	var  city string = "dhaka"
-	var c rune="a"
+	var c rune = 'a'
 	var age int = 10 
-	fmt.Println(name, rune, age)
+	fmt.Println(name, c, age)
 	fmt.Println(city)
 	fmt.Println("Git local to Remote")
 	fmt.Println("hello Bangladesh")
