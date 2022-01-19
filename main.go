b2c970f539531c82d7f8faac1f1c67ab4fc47092
Md Ferdous Alam <56306765+mdferdousalam@users.noreply.github.com>
2022-01-19 11:36:27 +0600

    version 3

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	var name string = "ferdous"
+	var name, city string = "dhaka", "ferdous"
 
 	fmt.Println("Git local to Remote")
 	fmt.Println("hello Bangladesh")
